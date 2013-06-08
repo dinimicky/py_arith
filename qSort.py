28afb46e10beecf12210c87532ab2259c54fc56c
dinimicky <dinimicky@gmail.com>
2013-06-08 20:44:41 +0800

    quick sort

---
+++ qSort.py
@@ -0,0 +1,29 @@
+'''
+Created on 2013-6-8
+
+@author: Brilliant
+'''
+Seq=list()
+Seq=range(1,100)+range(300,400)+range(200,300)
+Seq.reverse()
+LargeSeq=range(1,1000)+range( 3000, 4000)+range( 200, 3000)
+def qsort(List=[]):
+    if len(List)== 1 or len(List)== 0:
+        return List
+    else:
+        Pivot=List[ 0]
+        Upper=[]
+        Lower=[]
+        for Value in List[ 1:]:
+            if Value > Pivot:
+                Upper.append(Value)
+            else:
+                Lower.append(Value)
+        return qsort(Lower)+[Pivot]+qsort(Upper)
+       
+
+
+
+print qsort(Seq)
+       
+  
