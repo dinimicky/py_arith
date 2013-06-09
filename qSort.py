b1fa8c2844f2f4e920f6c541cbc8060247b82aa5
dinimicky <dinimicky@gmail.com>
2013-06-09 21:07:09 +0800

    try to solve dirty

--- qSort.py
+++ qSort.py
@@ -63,16 +63,8 @@ def qSort2(List):
     
       
 print Seq
-<<<<<<< Upstream, based on origin/master
 print qSort(Seq)
 print qSort2(Seq)
 print len(LargeSeq)
 print qSort2(LargeSeq)
 
-
-  
-=======
-print qsort(Seq)
-# print LargeSeq
-# print qsort(LargeSeq)
->>>>>>> dc4c77e get new qSort file
