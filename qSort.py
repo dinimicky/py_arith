8fe4f45ff16fca45875235ee0b9fb087b2caa649
dinimicky <dinimicky@gmail.com>
2013-06-09 16:19:47 +0800

    add the partition function to simply the qSort function add the non-recursive quick sort function named qSort2

--- qSort.py
+++ qSort.py
@@ -3,29 +3,70 @@ Created on 2013-6-8
 
 @author: Brilliant
 '''
+import types
 Seq=list()
 Seq=range(1,100)+range(300,400)+range(200,300)
 Seq.reverse()
-LargeSeq=range(1,1000)+range( 3000, 4000)+range( 200, 3000)
-def qsort(List=[]):
-    if len(List)== 1 or len(List)== 0:
-        return List
-    else:
-        Pivot=List[ 0]
-        Upper=[]
-        Lower=[]
-        for Value in List[ 1:]:
-            if Value > Pivot:
-                Upper.append(Value)
-            else:
-                Lower.append(Value)
-        return qsort(Lower)+[Pivot]+qsort(Upper)
-       
+LargeSeq=range(1000,1, -1)+range( 3000, 4000)+range( 200, 3000)
+def partition(List=[]):
+    if List == []:
+        return [], None, []
+    if len(List) == 1:
+        return [], List[0], []
+    
+    Upper =[]
+    Lower = []    
+    
+    Pivot = List[0]
+    RestList = List[1:]
+    for value in RestList:
+        if value > Pivot:
+            Upper.append(value)
+        else:
+            Lower.append(value)
+            
+    return Lower, Pivot, Upper  
 
+def qSort(List=[]):
+    Lower, Pivot, Upper = partition(List)
+    if Pivot == None:
+        return []
+    
+    return qSort(Lower)+[Pivot]+qSort(Upper)
 
+          
+def qSort2(List):
+    Result = []
+    Stack = []
+    Lower, Pivot, Upper = partition(List)
+    Stack.append(Upper)
+    Stack.append(Pivot)
+    Stack.append(Lower)
+    while True:
+        if Stack != []:
+            Top = Stack.pop()
+        else:
+            break
+        if types.ListType == type(Top):
+            if Top == []:
+                continue
+            Lower, Pivot, Upper = partition(Top)
+            Stack.append(Upper)
+            Stack.append(Pivot)
+            Stack.append(Lower)
+        else:
+            if Top != None:
+                Result.append(Top)
+                
+    return Result
+            
+    
+      
 print Seq
-print qsort(Seq)
-# print LargeSeq
-# print qsort(LargeSeq)
-       
+print qSort(Seq)
+print qSort2(Seq)
+print len(LargeSeq)
+print qSort2(LargeSeq)
+
+
   
