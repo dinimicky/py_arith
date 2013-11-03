80fa47646c675eefdab1253f4e3289ea4f988e12
dinimicky <dinimicky@gmail.com>
2013-11-03 17:28:57 +0800

    realize the permutation & combination function

---
+++ perm_comb.py
@@ -0,0 +1,16 @@
+'''
+Created on 2013-11-2
+
+@author: Brilliant
+'''
+
+def permutation(List, Number):
+    pass
+
+
+
+def combination(List, Number):
+    pass
+
+if __name__ == '__main__':
+    pass
