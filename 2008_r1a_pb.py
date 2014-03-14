20c4ae7ecf6d8332592bb71f81ab8d580b39b57a
ezonghu <ezonghu@E7D4C9EFE945D2.ericsson.se>
2014-03-14 21:01:36 +0800

    2008 r1a problem B solved

--- 2008_r1a_pb.py
+++ 2008_r1a_pb.py
@@ -3,7 +3,42 @@ Created on 2014-3-11
 
 @author: ezonghu
 '''
-fp = open('A-small-practice.in')
+fp = open('B-large-practice.in')
+
+def checkX(CF, X):
+    Malted, UnMalted=CF
+    if UnMalted != []:
+        for T in UnMalted:
+            if 0 == X[T]:
+                return True
+    if Malted != None:
+        if 1 == X[Malted]:
+            return True
+    return False
+
+def initX(Types):
+    return {i:0 for i in xrange(1,Types+1)}
+            
+def checkXModification(PreCFs, X):
+    for CF in PreCFs:
+        if not checkX(CF, X):
+            return False
+    return True
+
+def tryX(CustomersFlavors, X):
+    for i in xrange(len(CustomersFlavors)):
+        CF = CustomersFlavors[i]
+        if checkX(CF,X):
+            continue
+        Malted, Umalted = CF
+        if Malted == None and Umalted != []:
+            return False
+        
+        if Malted != None:
+            X[Malted]=1
+            return tryX(CustomersFlavors, X)
+        
+    return X  
 
 first_line = True
 CaseNum = 0
@@ -17,18 +52,43 @@ for line in fp:
     if CaseLine == 0:
         CaseLine +=1
         CaseItem +=1
-        VectorNum = int(line)
+        Types = int(line)
         continue
     if CaseLine == 1:
         CaseLine +=1
-        Vector1 = [int(i) for i in line.split()]
-        Vector1.sort()
+        Customers = int(line)
+        CustomersFlavors = []
         continue
-    if CaseLine == 2:
+    if CaseLine >= 2:
         CaseLine += 1
-        Vector2 = [int(i) for i in line.split()]
-        Vector2.sort(reverse=True)
-        Sum = 0
-        for i,j in zip(Vector1, Vector2):
-            Sum += i*j
-        print "Case #%d: %d" % (CaseItem, Sum)
+        Customers -= 1
+        OneCustomerFlavor = [int(i) for i in line.split()]
+        Ts = []
+        Tm = None
+        for i in xrange(OneCustomerFlavor[0]):
+            T = OneCustomerFlavor[i*2+1]
+            M = OneCustomerFlavor[i*2+2]
+            if 1==M:
+                Tm=T
+                continue
+            Ts.append(T)
+        CustomersFlavors.append((Tm, Ts))
+        
+        if Customers == 0:
+            CaseLine = 0
+            
+            Str = 'Case #%d: %s'
+            if CaseItem==2:
+                pass
+            Res2 = tryX(CustomersFlavors, initX(Types))
+            def printRes(Res):
+                if Res==False: 
+                    print Str % (CaseItem, 'IMPOSSIBLE')
+                else:
+                    ResStr=''
+                    for i in xrange(1,Types+1):
+                        ResStr +=' %d' % Res[i]
+                    print Str % (CaseItem, ResStr)
+            printRes(Res2)
+            CustomersFlavors=[]
+
