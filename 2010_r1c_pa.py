db6d85750221afa4564cbba04486186ab08d22fd
ezonghu <ezonghu@E7D4C9EFE945D2.ericsson.se>
2014-03-19 04:04:30 +0800

    2010 r1c problem a solved

---
+++ 2010_r1c_pa.py
@@ -0,0 +1,34 @@
+'''
+Created on 2014-3-19
+
+@author: ezonghu
+'''
+
+def CountCross(lines):
+    total = len(lines)
+    Counter = 0
+    for i in xrange(total-1):
+        [CurrL, CurrR] = lines[i]
+        for [NextL, NextR] in lines[i+1:]:
+            if CurrL<NextL and CurrR>NextR:
+                Counter += 1
+            if CurrL>NextL and CurrR<NextR:
+                Counter +=1
+    return Counter
+            
+
+f=open('A-large-practice.in')
+first_line = f.readline()
+Cases = int(first_line)
+CaseId = 0
+for s in f:
+    CaseId += 1
+    lineNum = int(s)
+    lines = []
+    for l in f:
+        lines.append([int(i) for i in l.split()])
+        lineNum-=1
+        if lineNum==0:
+            break
+    print "Case #%d: %d" % (CaseId, CountCross(lines))
+f.close()
