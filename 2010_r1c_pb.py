54bd96cce0836cb23998a71ca26c68f8f9a32eca
ezonghu <ezonghu@E7D4C9EFE945D2.ericsson.se>
2014-03-22 19:40:28 +0800

    2010 r1c problem b solved

---
+++ 2010_r1c_pb.py
@@ -0,0 +1,28 @@
+'''
+Created on 2014-3-21
+
+@author: ezonghu
+'''
+
+
+def CounterWorst(L,P,C):
+    if L*C >=P:
+        return 0
+    if L*C*C >= P :
+        return 1
+    
+    import math
+    N=(math.log(P)-math.log(L))/math.log(C)
+    N=math.ceil(N)
+    X=L*math.pow(C,N//2)
+    return 1+max( CounterWorst(L,X,C), CounterWorst(X,P,C))
+ 
+f = open('B-large-practice.in')
+first_line = f.readline()
+Cases = int(first_line)
+CaseNo = 0
+ 
+for l in f:
+    CaseNo += 1
+    [L,P,C]=[int(i)for i in l.split()]
+    print "Case #%d: %d" % (CaseNo, CounterWorst(L,P,C))
