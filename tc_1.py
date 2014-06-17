18e5dbfd5390fb8dd08ddc5ca0300684d01aec55
ezonghu <ezonghu@E7D4C9EFE945D2.ericsson.se>
2014-06-18 02:10:22 +0800

    add 1 pure math soltion to solve the problem

--- tc_1.py
+++ tc_1.py
@@ -1,14 +1,37 @@
-'''
-Created on 2014-5-28
-
-@author: ezonghu
-'''
-'''
-Created on 2014-5-28
-
-@author: ezonghu
-'''
 import math
+
+class Lottery2(object):
+    def __parse_rules(self, rule):
+        [Name,Others] = rule.split(":")
+        res = Others.strip().split(" ")
+        [Choices, Blanks, Sorted, Unique] =  res
+        return Name, int(Choices), int(Blanks), Sorted, Unique
+    def perm(self, Choices, Blanks):
+        return math.factorial(Choices) / math.factorial(Choices - Blanks)
+    def comb(self, Choices, Blanks):
+        return self.perm(Choices, Blanks) / math.factorial(Blanks)
+    def __get_odds(self, Choices, Blanks, Sorted, Unique):
+        if Sorted == "F" and Unique == "F":
+            return Choices ** Blanks
+
+        if Sorted == "F" and Unique == "T":
+            return self.perm(Choices, Blanks)
+        if Sorted == "T" and Unique == "F":
+            return sum(self.comb(Choices, i) * self.comb(Blanks - 1, i - 1) for i in range(1, Blanks + 1))
+        if Sorted == "T" and Unique == "T":
+            return self.comb(Choices, Blanks)
+        
+    def sortByOdds(self, rules):
+        Name_Odds = []
+        for rule in rules:
+            Name, Choices, Blanks, Sorted, Unique = self.__parse_rules(rule)
+            Odds = self.__get_odds(Choices, Blanks, Sorted, Unique)
+            Name_Odds.append((Name, Odds))
+        from operator import itemgetter
+        sorted_Name_Odds = sorted(Name_Odds, key = itemgetter(1,0), reverse=False)
+        print(sorted_Name_Odds)
+        return tuple(Name for Name, _Odds in sorted_Name_Odds)
+    
 class Lottery(object):
     def __parse_rules(self, rule):
         [Name,Others] = rule.split(":")
@@ -74,4 +97,4 @@ def perm(n, r):
     return sum(perm(n - 1, r - 1) for _x in range(n))
 print(comb_with_replacement(100, 8)) 
 print(comb(100, 8))    
-print(perm(100,8))
+print(perm(100, 8))
