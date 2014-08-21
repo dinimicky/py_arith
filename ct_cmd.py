5f67ec1cc11bd65dd0ece87270346dec8a479f6f
ezonghu <ezonghu@E7D4C9EFE945D2.ericsson.se>
2014-08-22 04:35:46 +0800

    test datetime function get calculate the delta time

--- ct_cmd.py
+++ ct_cmd.py
@@ -5,8 +5,21 @@ create a shell file or run the command:
 
 cleartool setview -exec /home/ezonghu/ct.py ezonghu_mgc_pdb
 '''
+
+time_str0 = '20070523.101629'
+time_str1 = '20070623.101629'
+time_fmt = "%Y%m%d.%H%M%S"
+get_datetime = lambda time_str : datetime.datetime.strptime(time_str, time_fmt)
+import datetime
+dt = get_datetime(time_str0)
+dt2 = get_datetime(time_str1)
+print(dt)
+delta = dt2-dt
+print(delta)
+print(delta.total_seconds())
+print(datetime.datetime.now())
 def run_cmd(cmd):
-    print( cmd)
+    print(cmd)
     import subprocess
     proc = subprocess.Popen([cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return proc.communicate()
@@ -32,6 +45,7 @@ import os
 for r, ds, fs in os.walk("."):
     for d in ds:
         print(r+d)
+
 
 
 
